aec2e7f0d2f5e3457862918a16104b659338125e
dheeraj <dheeraj.07april@gmail.com>
2018-02-10 11:57:27 -0500

    modify UI

--- learn_react/src/App.js
+++ learn_react/src/App.js
@@ -6,15 +6,39 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <Biography />
+          <GroceryList />
       </div>
     );
+  }
+}
+
+class Biography extends Component {
+  render() {
+    return(
+      <div>
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Dheeraj Mendiratta</p>
+          <p>Skill:- node, react</p>
+          <hr/>
+      </div>
+    )
+  }
+}
+
+class GroceryList extends Component {
+  render() {
+    return (
+      <div>
+        <p>Grocery List</p>
+        <ul>
+          <li>Coffee</li>
+          <li>Tea</li>
+          <li>Milk</li>
+        </ul>
+        <hr/>
+      </div>
+    )
   }
 }
 
